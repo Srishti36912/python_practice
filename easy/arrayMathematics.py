7d70c4525b95bd1d44c69899d7e76e4aecf7c4b0
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-10-24 23:22:53 +0530

    finding the addition, subtraction, multiplication, division, modulus and power of the two arrays

---
+++ easy/arrayMathematics.py
@@ -0,0 +1,12 @@
+import numpy
+
+N, M = map(int, input().split())
+A = numpy.array([input().split() for i in range(N)], int)
+B = numpy.array([input().split() for i in range(N)], int)
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+print(A ** B)
